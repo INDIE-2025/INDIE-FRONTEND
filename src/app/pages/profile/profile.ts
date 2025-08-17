6504c412003bea570be766c22974b9affb647bec
Agustina Linares <143526107+aguslinares@users.noreply.github.com>
2025-08-16 21:04:20 -0300

    Agregados

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Carousel } from "../../components/carousel/carousel";
+import { Calendar } from "../../components/calendar/calendar";
+import { Comments } from "../../components/comments/comments";
 
 @Component({
   selector: 'app-profile',
-  imports: [Carousel],
+  imports: [Carousel, Calendar, Comments],
   templateUrl: './profile.html',
   styleUrl: './profile.scss'
 })
