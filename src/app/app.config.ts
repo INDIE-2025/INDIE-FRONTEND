9d6a17ab96b2b4937bb7f7e4422277e971652a1c
Lucas Ortiz <yoshixd444@gmail.com>
2025-08-27 12:35:27 -0300

    feat: Update Angular dependencies and add Material theming support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
 import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
