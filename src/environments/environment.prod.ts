d1bb85cf130e860b24ed4d9788ce6fc4f6fc77bb
Lucas Ortiz <yoshixd444@gmail.com>
2025-09-24 20:36:30 -0300

    fix: Update apiUrl to use HTTPS in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts (prod)
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-18-230-57-225.sa-east-1.compute.amazonaws.com:8080'
+  apiUrl: 'https://ec2-18-230-57-225.sa-east-1.compute.amazonaws.com'
 };
